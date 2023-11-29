8a5e200d4223e3ef0080f711af3999697256c2a0
isoonissalty <isoon.ph@gmail.com>
2023-11-29 23:49:09 +0700

    handle lost directory

--- get-cov-diff.sh
+++ get-cov-diff.sh
@@ -8,48 +8,41 @@ fi
 # store current git branch name
 current_branch=$(git branch --show-current)
 
-BASEDIR=$(dirname "$0")
-echo "BASEDIR: $BASEDIR"
+target_branch=$1
 
-BASEDIR1=$(realpath "$0")
-echo "BASEDIR: $BASEDIR1"
+root_dir=$0
 
-BASEDIR2=$(realpath "$0/get-cov-diff")
-echo "BASEDIR: $BASEDIR2"
+# Display the provided target branches
+echo "compare $current_branch branch to $target_branch"
 
-# target_branch=$1
+# Run nvm use (if nvm is installed)
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
 
-# # Display the provided target branches
-# echo "compare $current_branch branch to $target_branch"
+nvm use
+npm i
 
-# # Run nvm use (if nvm is installed)
-# export NVM_DIR=$HOME/.nvm;
-# source $NVM_DIR/nvm.sh;
+# Run npm run test:coverage and append output to a file
+npm run test:coverage > $root_dir/coverage/branch_current_coverage.txt
 
-# nvm use
-# npm i
+# clean up change files after install (package-lock.json)
+git restore .
 
-# # Run npm run test:coverage and append output to a file
-# npm run test:coverage > $BASEDIR/coverage/branch_current_coverage.txt
+# Checkout the second target branch
+git checkout $target_branch
+git pull origin $target_branch
 
-# # clean up change files after install (package-lock.json)
-# git restore .
+nvm use
+npm i
 
-# # Checkout the second target branch
-# git checkout $target_branch
-# git pull origin $target_branch
+# Run npm run test:coverage and append output to a file
+npm run test:coverage > $root_dir/coverage/branch_target_coverage.txt
 
-# nvm use
-# npm i
+nvm use stable
+# running index.js in this file directory even calling from another directory
+node $root_dir/index.js $root_dir/coverage/branch_current_coverage.txt $root_dir/coverage/branch_target_coverage.txt
 
-# # Run npm run test:coverage and append output to a file
-# npm run test:coverage > $BASEDIR/coverage/branch_target_coverage.txt
-
-# nvm use stable
-# # running index.js in this file directory even calling from another directory
-# node $BASEDIR/index.js $BASEDIR/coverage/branch_current_coverage.txt $BASEDIR/coverage/branch_target_coverage.txt
-
-# # clean up change files after install (package-lock.json)
-# git restore .
-# git checkout $current_branch
-# # End of the script
+# clean up change files after install (package-lock.json)
+git restore .
+git checkout $current_branch
+# End of the script
