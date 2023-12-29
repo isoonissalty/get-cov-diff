3e6f28aa1c82575a184925408e134c327247f7c1
isoonissalty <isoon.ph@gmail.com>
2023-12-29 14:57:32 +0700

    adjust script to run only changed file with jest

--- get-cov-diff.sh
+++ get-cov-diff.sh
@@ -26,6 +26,12 @@ fi
 # Display the provided target branches
 echo "compare $current_branch branch to $target_branch"
 
+git diff --name-only $current_branch $target_branch > $BASEDIR/changed_files.txt
+
+cat $BASEDIR/changed_files.txt | sed -n -e '/\.test\.js$/p' | tr '\n' ' ' > $BASEDIR/changed_test_files.txt
+
+cat $BASEDIR/changed_files.txt | sed -n -e '/\.test\.js/!p' | tr '\n' ',' > $BASEDIR/changed_non_test_files.txt
+
 # Run nvm use (if nvm is installed)
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
@@ -34,7 +40,7 @@ nvm use
 npm i
 
 # Run npm run test:coverage and append output to a file
-npm run test:coverage > $BASEDIR/branch_current_coverage.txt
+npm run test:coverage -- $(cat $BASEDIR/changed_test_files.txt) --collectCoverageFrom={$(cat $BASEDIR/changed_non_test_files.txt)} > $BASEDIR/branch_current_coverage.txt
 
 # clean up change files after install (package-lock.json)
 git restore .
@@ -43,13 +49,11 @@ git restore .
 git checkout $target_branch
 git pull origin $target_branch
 
-git diff --name-only $current_branch $target_branch > $BASEDIR/changed_files.txt
-
 nvm use
 npm i
 
 # Run npm run test:coverage and append output to a file
-npm run test:coverage > $BASEDIR/branch_target_coverage.txt
+npm run test:coverage -- $(cat $BASEDIR/changed_test_files.txt) --collectCoverageFrom={$(cat $BASEDIR/changed_non_test_files.txt)} > $BASEDIR/branch_target_coverage.txt
 
 nvm use stable
 # running index.js in this file directory even calling from another directory
